97a1a0c866ea93dd2596e89ba97b0543263acbea
YOGESH L <92539127+Yogesh2460@users.noreply.github.com>
2022-08-24 23:11:28 +0530

    Update extension.py

--- extension.py
+++ extension.py
@@ -1,3 +1,7 @@
 filename=input("Input the Filename: ")
 f_extns=filename.split(".")
 print("The extension of the file is : " + repr(f_extns[-1]))
+#cap i made this so that it prints extensions of all types file
+#if I want to make the code print 'python' for py then
+if f_extns[-1] == 'py':
+  print("The extension of the file is : python ")
